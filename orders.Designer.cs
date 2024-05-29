277bce9f5177e786265ef0bfb22ccbcc8a3615b4
EllaMillares02 <ellamaemillares20@gmail.com>
2024-05-29 11:38:42 +0800

    Add project files.

---
+++ orders.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace OrderingSystem
+{
+    partial class orders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(orders));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.getBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.postBTN = new System.Windows.Forms.Button();
+            this.quantityTXT = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.phoneNumTXT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addressTXT = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTXT = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(895, 204);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // getBTN
+            // 
+            this.getBTN.Location = new System.Drawing.Point(567, 295);
+            this.getBTN.Name = "getBTN";
+            this.getBTN.Size = new System.Drawing.Size(357, 48);
+            this.getBTN.TabIndex = 1;
+            this.getBTN.Text = "Get Orders";
+            this.getBTN.UseVisualStyleBackColor = true;
+            this.getBTN.Click += new System.EventHandler(this.getBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer Order";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Add Order";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.postBTN);
+            this.panel1.Controls.Add(this.quantityTXT);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.phoneNumTXT);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.addressTXT);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.nameTXT);
+            this.panel1.Location = new System.Drawing.Point(29, 387);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(895, 281);
+            this.panel1.TabIndex = 4;
+            // 
+            // postBTN
+            // 
+            this.postBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(232)))), ((int)(((byte)(245)))));
+            this.postBTN.Location = new System.Drawing.Point(195, 210);
+            this.postBTN.Name = "postBTN";
+            this.postBTN.Size = new System.Drawing.Size(506, 48);
+            this.postBTN.TabIndex = 5;
+            this.postBTN.Text = "Save Order";
+            this.postBTN.UseVisualStyleBackColor = false;
+            this.postBTN.Click += new System.EventHandler(this.postBTN_Click);
+            // 
+            // quantityTXT
+            // 
+            this.quantityTXT.Location = new System.Drawing.Point(601, 142);
+            this.quantityTXT.Name = "quantityTXT";
+            this.quantityTXT.Size = new System.Drawing.Size(256, 26);
+            this.quantityTXT.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(527, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Quantity";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(538, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(319, 28);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "SELECT PRODUCT";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(534, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Product Order";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Phone Number";
+            // 
+            // phoneNumTXT
+            // 
+            this.phoneNumTXT.Location = new System.Drawing.Point(157, 142);
+            this.phoneNumTXT.Name = "phoneNumTXT";
+            this.phoneNumTXT.Size = new System.Drawing.Size(262, 26);
+            this.phoneNumTXT.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // addressTXT
+            // 
+            this.addressTXT.Location = new System.Drawing.Point(157, 81);
+            this.addressTXT.Name = "addressTXT";
+            this.addressTXT.Size = new System.Drawing.Size(262, 26);
+            this.addressTXT.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Customer Name";
+            // 
+            // nameTXT
+            // 
+            this.nameTXT.Location = new System.Drawing.Point(157, 25);
+            this.nameTXT.Name = "nameTXT";
+            this.nameTXT.Size = new System.Drawing.Size(262, 26);
+            this.nameTXT.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::OrderingSystem.Properties.Resources.bakery_shop;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(29, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // orders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(232)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(950, 693);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getBTN);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "orders";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Orders";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button getBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox phoneNumTXT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox addressTXT;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTXT;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button postBTN;
+        private System.Windows.Forms.TextBox quantityTXT;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ orders.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace OrderingSystem
+{
+    public partial class orders : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public orders()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+
+        public class Product
+        {
+            public int ProductId { get; set; }
+            public string ProdName { get; set; }
+            public decimal ProdPrice { get; set; }
+
+            public override string ToString()
+            {
+                return ProdName;
+            }
+        }
+
+        private async void LoadProducts()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost/myapi/phpapi/apiOrders.php?action=getProducts");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON response into a list of Product objects
+                var products = JsonConvert.DeserializeObject<List<Product>>(responseBody);
+
+                comboBox1.Items.Clear();
+                foreach (var product in products)
+                {
+                    comboBox1.Items.Add(product);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading products: " + ex.Message);
+            }
+        }
+
+        public class Order
+        {
+            public int OrderID { get; set; }
+            public string CustName { get; set; }
+            public string CustAddress { get; set; }
+            public string PhoneNum { get; set; }
+            public string ProdName { get; set; }
+            public decimal ProdPrice { get; set; }
+            public int ProductID { get; set; }
+            public int Quantity { get; set; }
+            public double AmountDue { get; set; }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private async void postBTN_Click(object sender, EventArgs e)
+        {
+            var selectedProduct = comboBox1.SelectedItem as Product;
+
+            if (selectedProduct == null)
+            {
+                MessageBox.Show("Please select a product.");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(nameTXT.Text) ||
+                string.IsNullOrWhiteSpace(addressTXT.Text) ||
+                string.IsNullOrWhiteSpace(phoneNumTXT.Text) ||
+                string.IsNullOrWhiteSpace(quantityTXT.Text))
+            {
+                MessageBox.Show("All fields are required.");
+                return;
+            }
+
+            var userData = new
+            {
+                custName = nameTXT.Text,
+                custAddress = addressTXT.Text,
+                phoneNum = phoneNumTXT.Text,
+                productID = selectedProduct.ProductId,
+                quantity = int.Parse(quantityTXT.Text),
+                prodPrice = selectedProduct.ProdPrice,  
+
+            };
+
+            string json = JsonConvert.SerializeObject(userData);
+            Console.WriteLine("JSON Sent: " + json); // Log the JSON payload
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync("http://localhost/myapi/phpapi/apiOrders.php", content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                MessageBox.Show(responseBody);
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show("Request error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            nameTXT.Text = "";
+            addressTXT.Text = "";
+            phoneNumTXT.Text = "";
+            comboBox1.Text = "SELECT PRODUCT";
+            quantityTXT.Text = "";
+        }
+
+        private async void getBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost/myapi/phpapi/apiOrders.php");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var orders = JsonConvert.DeserializeObject<List<Order>>(responseBody);
+
+                dataGridView1.DataSource = orders;
+                // Adjust columns to show only necessary data
+                dataGridView1.Columns["ProductID"].Visible = false;
+                dataGridView1.Columns["ProdName"].HeaderText = "Product Name";
+                dataGridView1.Columns["ProdPrice"].HeaderText = "Product Price";
+                dataGridView1.Columns["AmountDue"].HeaderText = "Amount Due";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
